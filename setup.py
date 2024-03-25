80e3db773bc612e0308a737c10f15a7a84dd37d3
chiasmod0n <ah674092@gmail.com>
2024-03-25 05:23:51 +0300

    update

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='chiasmodon',
-      version='0.2.10',
+      version='0.2.12',
       description='Chiasmodon is an OSINT (Open Source Intelligence) tool designed to assist in the process of gathering information about a target domain. Its primary functionality revolves around searching for domain-related data, including domain emails, domain credentials (usernames and passwords), CIDRs (Classless Inter-Domain Routing), ASNs (Autonomous System Numbers), and subdomains..',
       long_description=long_description,
       long_description_content_type='text/markdown',
       author='chiasmod0n',
-      keywords='Chiasmodon OSINT tool Open Source Intelligence Domain search Domain emails Domain credentials User Passwords CIDRs ASNs Subdomains Reconnaissance Security assessment Digital footprint Network infrastructure Attack surface Information gathering Target domain Email search Credential search Network reconnaissance',
+      keywords='intelligence osint credentials emails asn cidr bugbounty subdomains information-gathering intelligence-analysis reconnaissance attack-surface subdomain-enumeration reconnaissance-framework bugbounty-tool email-enumeration chiasmodon',
       url='https://github.com/chiasmod0n/chiasmodon',
       packages=['.'],
       scripts=['cli/chiasmodon_cli.py'],
