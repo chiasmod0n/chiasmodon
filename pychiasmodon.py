5d6da6773491a7acc15662f73b4d560daf4c29e4
chiasmod0n <ah674092@gmail.com>
2024-03-31 05:05:36 +0300

    update

--- pychiasmodon.py
+++ pychiasmodon.py
@@ -5,7 +5,7 @@ import requests
 import tldextract
 from yaspin import Spinner
 
-VERSION = "0.2.17"
+VERSION = "0.2.18"
 
 class Chiasmodon:
     API_URL         = 'https://chiasmodon.club/v2/api/beta'
