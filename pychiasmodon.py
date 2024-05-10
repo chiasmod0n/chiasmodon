9225124470a17292a280ed12d9ed031ed4f8e0f2
chiasmod0n <ah684092@gmail.com>
2024-05-10 13:53:04 +0300

    update

--- pychiasmodon.py
+++ pychiasmodon.py
@@ -5,7 +5,7 @@ import time
 import requests
 from yaspin import Spinner 
 
-VERSION = "2.0.5"
+VERSION = "2.0.6"
 _API_URL = 'https://chiasmodon.com/v2/api/beta'
 _API_HEADERS = {'user-agent':'cli/python'}
 _VIEW_TYPE = {
